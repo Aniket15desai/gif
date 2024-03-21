b28fb0b5919fcda2c9cb05b695d2f0dd413a1076
aniket15desai <aniketsardesai44@gmail.com>
2024-03-21 17:32:40 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,6 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen bg-homeBg bg-cover lg:bg-contain w-screen bg-center flex-col items-center justify-between">
+    </main>
+  );
+}
